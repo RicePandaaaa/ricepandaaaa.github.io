232914b5dc8317deea00d42a8a2c09d650a29b2e
Ricepandaaaa <anthony.ha.pham@gmail.com>
2024-12-22 07:29:49 -0600

    Add footer, change background, change intro text

--- scripts.js
+++ scripts.js
@@ -15,18 +15,13 @@ function typeText(element, text, speed, callback) {
   document.addEventListener("DOMContentLoaded", () => {
     const line1 = document.getElementById("line1");
     const line2 = document.getElementById("line2");
-    const line3 = document.getElementById("line3");
-  
     // Clear content to simulate typing
     line1.textContent = ">> ";
     line2.textContent = ">> ";
-    line3.textContent = ">> ";
   
     // Type each line one by one
     typeText(line1, "Howdy! I'm Anthony Pham!", 20, () => {
-      typeText(line2, "I'm a multidisciplinary (mechatronics) engineer improving education through innovative software!", 1, () => {
-        typeText(line3, "Feel free to explore my lives as a student, a coder, a teacher, and a hobbyist! (Website is in renovation!)", 1);
-      });
+      typeText(line2, "Check out my different lives below! (Website under renovation!)", 20)
     });
   });
   
